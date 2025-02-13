97fcb771acf58f8c0e152b0ab865ad6f39330928
Marcelo-Allves <marcelo.allves13@gmail.com>
2025-02-13 12:54:56 +0000

    Criei o cadastro de Cartas do super trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,33 +1,110 @@
 #include <stdio.h>
 
-int main() {
-    char nome[30];
+    // Variaveis para "armazenar"
+
+int main() {   
     int populacao, pontos_turisticos;
-    float pib, area;
+    float area, pib;
+    char estado;
+    int cidade;
 
-    // Cadastro da cidade
-    printf("Nome da cidade: ");
-    scanf("%s", nome);  
-
-    printf("População: ");
+    // Cadastro da cidade A01
+    printf("Cadastro da cidade A01:\n");
+    printf("População (somente numeros): ");
     scanf("%d", &populacao);
-
-    printf("Área (km²): ");
+    printf("Área (em km²)(somente numeros): ");
     scanf("%f", &area);
-
-    printf("PIB (bilhões): ");
+    printf("PIB (em bilhões de R$): ");
     scanf("%f", &pib);
-
-    printf("Quantidade de pontos turísticos: ");
+    printf("Número de pontos turísticos: ");
     scanf("%d", &pontos_turisticos);
+    printf("\n");
 
-    //dados cadastrados
-    printf("\nDADOS DA CIDADE:\n");
-    printf("Nome: %s\n", nome);
-    printf("População: %d\n", populacao);
-    printf("Área: %f km²\n", area);
-    printf("PIB: %f bilhões\n", pib);
-    printf("Pontos turísticos: %d\n", pontos_turisticos);
+    // Cadastro da cidade A02
+    printf("Cadastro da cidade A02:\n");
+    printf("População (somente numeros): ");
+    scanf("%d", &populacao);
+    printf("Área (em km²)(somente numeros): ");
+    scanf("%f", &area);
+    printf("PIB (em bilhões de R$): ");
+    scanf("%f", &pib);
+    printf("Número de pontos turísticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("\n");
+
+    // Cadastro da cidade A03
+    printf("Cadastro da cidade A03:\n");
+    printf("População (somente numeros): ");
+    scanf("%d", &populacao);
+    printf("Área (em km²)(somente numeros): ");
+    scanf("%f", &area);
+    printf("PIB (em bilhões de R$): ");
+    scanf("%f", &pib);
+    printf("Número de pontos turísticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("\n");
+
+    // Cadastro da cidade A04
+    printf("Cadastro da cidade A04:\n");
+    printf("População (somente numeros): ");
+    scanf("%d", &populacao);
+    printf("Área (em km²)(somente numeros): ");
+    scanf("%f", &area);
+    printf("PIB (em bilhões de R$): ");
+    scanf("%f", &pib);
+    printf("Número de pontos turísticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("\n");
+
+    // Cadastro da cidade B01
+    printf("Cadastro da cidade B01:\n");
+    printf("População (somente numeros): ");
+    scanf("%d", &populacao);
+    printf("Área (em km²)(somente numeros): ");
+    scanf("%f", &area);
+    printf("PIB (em bilhões de R$): ");
+    scanf("%f", &pib);
+    printf("Número de pontos turísticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("\n");
+
+    // Cadastro da cidade B02
+    printf("Cadastro da cidade B02:\n");
+    printf("População (somente numeros): ");
+    scanf("%d", &populacao);
+    printf("Área (em km²)(somente numeros): ");
+    scanf("%f", &area);
+    printf("PIB (em bilhões de R$): ");
+    scanf("%f", &pib);
+    printf("Número de pontos turísticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("\n");
+
+    // Cadastro da cidade B03
+    printf("Cadastro da cidade B03:\n");
+    printf("População (somente numeros): ");
+    scanf("%d", &populacao);
+    printf("Área (em km²)(somente numeros): ");
+    scanf("%f", &area);
+    printf("PIB (em bilhões de R$): ");
+    scanf("%f", &pib);
+    printf("Número de pontos turísticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("\n");
+
+    // Cadastro da cidade B04
+    printf("Cadastro da cidade B04:\n");
+    printf("População (somente numeros): ");
+    scanf("%d", &populacao);
+    printf("Área (em km²) (somente numeros): ");
+    scanf("%f", &area);
+    printf("PIB (em bilhões de R$): ");
+    scanf("%f", &pib);
+    printf("Número de pontos turísticos: ");
+    scanf("%d", &pontos_turisticos);
+    printf("\n");
 
     return 0;
 }
+
+// Vai indo ate H04, mas nao entendi muito bem se era para ir
